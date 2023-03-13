c288674aaba6a46620e290649770757d9a3d49ea
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2023-03-14 02:26:51 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import Product from "./Pages/Product/Product";
+import Products from "./Pages/Products/Products";
+
+
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  )
+
+}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/products/:id",
+        element: <Products></Products>
+      },
+      {
+        path: "/product/:id",
+        element: <Product />
+      }
+    ]
+  }
+
+])
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='wrapper'>
+                <div className='left'>
+
+
+                </div>
+                <div className='center'></div>
+                <div className='right'></div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
